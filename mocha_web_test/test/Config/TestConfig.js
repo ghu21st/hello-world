eaa447a8554f47b5a99824d7259d814777627694
ghu21st <ghu21st@gmail.com>
2014-12-10 03:29:01 +0000

    update_angularjs_test

--- mocha_web_test/test/Config/TestConfig.js
+++ mocha_web_test/test/Config/TestConfig.js
@@ -2,8 +2,8 @@
 exports.options = {
 //  server            : "10.3.41.54",             	//test server host IP/name
 
-	port              : 8000,                             //default http website port
-//  port              : 8080,                           //default user customized app port
+//	port              : 8000,                             //default http website port
+  port              : 8080,                           //default user customized app port
 //	port              : 443,                            //default https website port
 //	test_location     : "/",      //test base folder
 //	test_location	  : "/home/ubuntu/workspace/mocha_web_test/test",
@@ -11,7 +11,7 @@ exports.options = {
 
 //	test_Url          : "https://angularjs.org/", 				//for public web side sample testing
 //	test_Url          : "https://hello-world-ghu21st.c9.io/", 	//for user customized nodejs app testing 
-	test_Url		  : "http://localhost:8000/app/index.html", //for nodejs phone app test
+	test_Url		  : "http://localhost:8080/app/index.html", //for nodejs phone app test
 
 	selenium_server   : "http://localhost:4444/wd/hub",    		//selenium server URL
   browser_driver    : "firefox",            //config browser driver to "firefox". NOTE: need to start selenium standalone server + Firefox driver first (or batch file)
